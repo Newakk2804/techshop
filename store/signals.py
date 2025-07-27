3c27c9928d17488ff1ddb82bf2da361bd23432d4
Newakk2804 <dan28.04@mail.ru>
2025-07-27 15:33:26 +0300

    added signals for deleting a product and category, related images from the media folder are deleted along with the product

--- store/signals.py
+++ store/signals.py
@@ -1,6 +1,8 @@
-from django.db.models.signals import post_save
+import os
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Product
+from django.conf import settings
+from .models import Product, Category
 from newsletters.tasks import send_new_product_email_task
 
 
@@ -8,3 +10,21 @@ from newsletters.tasks import send_new_product_email_task
 def trigger_product_email(sender, instance, created, **kwargs):
     if created:
         send_new_product_email_task.delay(instance.slug)
+
+
+def delete_file(file_field):
+    if file_field and file_field.name != "" and os.path.isfile(file_field.path):
+        try:
+            os.remove(file_field.path)
+        except Exception as e:
+            print(f"Ошибка при удалении файла: {e}")
+
+
+@receiver(post_delete, sender=Product)
+def delete_product_image(sender, instance, **kwargs):
+    delete_file(instance.image)
+
+
+@receiver(post_delete, sender=Category)
+def delete_category_image(sender, instance, **kwargs):
+    delete_file(instance.image)
