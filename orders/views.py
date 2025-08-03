8b8a0d02426092a0f0e2c6c22296f14d62f5e4f6
Newakk2804 <dan28.04@mail.ru>
2025-08-03 22:16:01 +0300

    the logic for displaying the order window and placing an order has been implemented

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from orders.models import Order, OrderItem
+from orders.forms import OrderCreateForm
+from carts.models import CartItem
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def order_create(request):
+    cart_items = CartItem.objects.filter(user=request.user)
+    total_price = request.user.get_cart_total_price()
+    total_quantity = request.user.get_cart_total_quantity()
+
+    if request.method == "POST":
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            order.save()
+
+            for item in cart_items:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item.product,
+                    price=item.product.final_price(),
+                    quantity=item.quantity,
+                )
+
+            cart_items.delete()
+
+            return redirect("orders:order_success", order_id=order.id)
+    else:
+        form = OrderCreateForm()
+
+    context = {
+        "form": form,
+        "cart_items": cart_items,
+        "total_price": total_price,
+        "total_quantity": total_quantity,
+    }
+
+    return render(request, "orders/order_create.html", context)
+
+
+def order_success(request, order_id):
+    context = {"order_id": order_id}
+
+    return render(request, "orders/order_success.html", context)
