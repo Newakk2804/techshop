1385573cbe02f77942654c72c5650cd56642ddc4
Newakk2804 <dan28.04@mail.ru>
2025-08-04 12:23:11 +0300

    added urls for orders user and for detail info order

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from orders.views import order_create, order_success
+from orders.views import order_create, order_success, user_orders, user_order_detail
 
 
 app_name = "orders"
@@ -7,4 +7,6 @@ app_name = "orders"
 urlpatterns = [
     path("create/", order_create, name="order_create"),
     path("success/<int:order_id>/", order_success, name="order_success"),
+    path("my-orders/", user_orders, name="user_orders"),
+    path("my-orders/<int:order_id>/", user_order_detail, name="user_order_detail"),
 ]
