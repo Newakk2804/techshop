126e4164661771b64e280eaab708bf555a8e4789
Newakk2804 <dan28.04@mail.ru>
2025-08-04 13:05:16 +0300

    added cache for categories

--- store/templatetags/category_tags.py
+++ store/templatetags/category_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.cache import cache
 from store.models import Category
 
 
@@ -7,4 +8,9 @@ register = template.Library()
 
 @register.simple_tag
 def get_all_categories():
-    return Category.objects.all()
+    categories = cache.get("all_categories")
+    if categories is None:
+        categories = list(Category.objects.all())
+        cache.set("all_categories", categories, 60 * 60)
+
+    return categories
