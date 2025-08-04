f5273e2f7962dff463917a352ff9fce9ad4f6bba
Newakk2804 <dan28.04@mail.ru>
2025-08-04 11:44:53 +0300

    added model payment in admin panel

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from payments.models import Payment
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "amount",
+        "currency",
+        "payment_method",
+        "status",
+        "created_at",
+        "paid_at",
+    )
+    list_filter = ("status", "payment_method", "currency", "created_at")
+    search_fields = ("order__id", "payment_id", "transaction_id")
+    readonly_fields = ("created_at", "updated_at", "paid_at")
